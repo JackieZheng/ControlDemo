eccd9f4e2cc052f5470e677dc5569f1a69138b16
apple <toutoumu@126.com>
2017-06-08 17:17:52 +0800

    修复bug https://stackoverflow.com/questions/35133047/swiperefreshlayout-and-tablayout-issue

--- app/src/main/java/liubin/com/myapplication/CheeseListFragment.java
+++ app/src/main/java/liubin/com/myapplication/CheeseListFragment.java
@@ -18,9 +18,10 @@ package liubin.com.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -32,45 +33,78 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.mylibrary.base.BaseFragment;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 public class CheeseListFragment extends BaseFragment {
 
+  @BindView(R.id.recyclerview) RecyclerView mRecyclerview;
+  @BindView(R.id.swip) SwipeRefreshLayout mSwipeRefreshLayout;
+  Unbinder unbinder;
+
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    final SwipeRefreshLayout refreshLayout =
-        (SwipeRefreshLayout) inflater.inflate(R.layout.fragment_cheese_list, container, false);
-    refreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
-        android.R.color.holo_green_light, android.R.color.holo_orange_light,
+    View view = inflater.inflate(R.layout.fragment_cheese_list, container, false);
+    unbinder = ButterKnife.bind(this, view);
+
+    mSwipeRefreshLayout.setColorSchemeResources(//
+        android.R.color.holo_blue_bright,//
+        android.R.color.holo_green_light,//
+        android.R.color.holo_orange_light,//
         android.R.color.holo_red_light);
-    refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+    mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
       @Override public void onRefresh() {
-        new Handler().postDelayed(new Runnable() {
-          @Override public void run() {
-            if (isViewCreated) {
-              refreshLayout.setRefreshing(false);
-            }
-          }
-        }, 4000);
+        Observable.timer(4000, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())// 指定之前的subscribe在io线程执行
+            .doOnSubscribe(new Consumer<Disposable>() {
+              @Override public void accept(Disposable disposable) throws Exception {
+                mDisposables.add(disposable);
+              }
+            })//开始执行之前的准备工作
+            .subscribeOn(AndroidSchedulers.mainThread())//指定 前面的doOnSubscribe 在主线程执行
+            .observeOn(AndroidSchedulers.mainThread())//指定 后面的subscribe在io线程执行
+            .subscribe(new Consumer<Long>() {
+              @Override public void accept(Long aLong) throws Exception {
+                if (isViewCreated) {
+                  mSwipeRefreshLayout.setRefreshing(false);
+                  mRecyclerview.getAdapter().notifyDataSetChanged();
+                }
+              }
+            });
       }
     });
-    RecyclerView rv = (RecyclerView) refreshLayout.findViewById(R.id.recyclerview);
-    setupRecyclerView(rv);
-    return refreshLayout;
+    setupRecyclerView(mRecyclerview);
+    return view;
   }
 
   @Override public void onDestroyView() {
     super.onDestroyView();
+    mSwipeRefreshLayout.removeAllViews();
+    unbinder.unbind();
   }
 
   private void setupRecyclerView(RecyclerView recyclerView) {
     recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-    recyclerView.setAdapter(new SimpleStringRecyclerViewAdapter(getActivity(),
-        getRandomSublist(Cheeses.sCheeseStrings, 30)));
+    //recyclerView.setAdapter(new SimpleStringRecyclerViewAdapter(getActivity(),
+    //  getRandomSublist(Cheeses.sCheeseStrings, 30)));
+    recyclerView.setAdapter(
+        new StringAdapter(R.layout.list_item, getRandomSublist(Cheeses.sCheeseStrings, 30), this));
   }
 
   private List<String> getRandomSublist(String[] array, int amount) {
@@ -80,6 +114,37 @@ public class CheeseListFragment extends BaseFragment {
       list.add(array[random.nextInt(array.length)]);
     }
     return list;
+  }
+
+  public static class StringAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+    private final int mBackground;
+    private final Fragment mFragment;
+
+    public StringAdapter(@LayoutRes int layoutResId, @Nullable List<String> data,
+        Fragment context) {
+      super(layoutResId, data);
+      this.mFragment = context;
+      final TypedValue mTypedValue = new TypedValue();
+      context.getContext()
+          .getTheme()
+          .resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
+      mBackground = mTypedValue.resourceId;
+    }
+
+    @Override protected void convert(BaseViewHolder helper, String item) {
+      helper.itemView.setBackgroundResource(mBackground);
+      helper.itemView.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+
+        }
+      });
+      helper.setText(android.R.id.text1, item);
+      ImageView imageView = helper.getView(R.id.avatar);
+      Glide.with(mFragment)
+          .load(Cheeses.getRandomCheeseDrawable())
+          .bitmapTransform(new CropCircleTransformation(mContext))
+          .into(imageView);
+    }
   }
 
   public static class SimpleStringRecyclerViewAdapter
@@ -106,10 +171,6 @@ public class CheeseListFragment extends BaseFragment {
       @Override public String toString() {
         return super.toString() + " '" + mTextView.getText();
       }
-    }
-
-    public String getValueAt(int position) {
-      return mValues.get(position);
     }
 
     public SimpleStringRecyclerViewAdapter(Context context, List<String> items) {
@@ -141,7 +202,7 @@ public class CheeseListFragment extends BaseFragment {
 
       Glide.with(holder.mImageView.getContext())
           .load(Cheeses.getRandomCheeseDrawable())
-          .fitCenter()
+          .bitmapTransform(new CropCircleTransformation(holder.mImageView.getContext()))
           .into(holder.mImageView);
     }
 
